21ddd27be7cc76d627410a9ac704b7f56692c99e
mojie <jeason8023@gmail.com>
2022-08-04 17:43:05 +0800

    feat(最终de): 最终的

--- .release-it.js
+++ .release-it.js
@@ -1,8 +1,15 @@
 module.exports = {
+  git: {
+    changelog:
+      "npx auto-changelog --stdout --commit-limit false --unreleased --template https://raw.githubusercontent.com/release-it/release-it/master/templates/changelog-compact.hbs",
+  },
+  hooks: {
+    "after:bump": "npx auto-changelog -p",
+  },
   plugins: {
     "@release-it/conventional-changelog": {
       infile: "CHANGELOG.md",
-      preset: "express"
+      preset: "conventionalcommits",
     },
   },
 };
