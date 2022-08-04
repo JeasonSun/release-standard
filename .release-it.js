67ab6234b1e66a04eb505cc47a4347aae240aa63
mojie <jeason8023@gmail.com>
2022-08-04 17:38:05 +0800

    feat: 1111

--- .release-it.js
+++ .release-it.js
@@ -1,15 +1,8 @@
-const fs = require("fs");
-
-const commitTemplate = fs.readFileSync("commit.hbs").toString();
-
 module.exports = {
   plugins: {
     "@release-it/conventional-changelog": {
       infile: "CHANGELOG.md",
-      preset: "angular",
-      writerOpts: {
-        commitPartial: commitTemplate,
-      },
+      preset: "ember"
     },
   },
 };
