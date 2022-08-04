c38c7fe812785627963deb6bc2c9484c096587b6
mojie <jeason8023@gmail.com>
2022-08-04 18:24:56 +0800

    feat: fsaf

--- .release-it.js
+++ .release-it.js
@@ -1,7 +1,7 @@
 module.exports = {
   git: {
     changelog:
-      "auto-changelog --stdout --commit-limit false --unreleased --template changeLogTemplate.hbs",
+      "auto-changelog --commit-limit false --unreleased --template changeLogTemplate.hbs",
   },
   hooks: {
     "after:bump": "auto-changelog -p",
