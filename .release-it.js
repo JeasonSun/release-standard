7f710a6df820d2479810fdf3954c6ddd0c5e7fb4
mojie <jeason8023@gmail.com>
2022-08-05 17:49:30 +0800

    Release 1.8.34

--- .release-it.js
+++ .release-it.js
@@ -3,9 +3,9 @@ const template = path.resolve(__dirname, 'changeLogTemplate.hbs');
 module.exports = {
   git: {
     changelog:
-      `auto-changelog --commit-limit false --unreleased --template json`,
+      `auto-changelog --commit-limit false --unreleased --template ${template}`,
   },
-  // hooks: {
-  //   "after:bump": `auto-changelog -p --template ${template}`,
-  // },
+  hooks: {
+    "after:bump": `auto-changelog -p --template ${template}`,
+  },
 };
